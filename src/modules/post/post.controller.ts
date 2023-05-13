f57397aa2a773fd151061545609fd929cb562ea3
Marek <sarkowag@gmail.com>
2023-05-13 21:46:32 +0200

    user likes, feed, profile etc.

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,119 +1,188 @@
-import { Controller, Get, Param, Req, UseGuards, Body, HttpStatus, Post, HttpCode, NotFoundException, ParseIntPipe } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    Req,
+    UseGuards,
+    Body,
+    HttpStatus,
+    Post,
+    HttpCode,
+    NotFoundException,
+    ParseIntPipe,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostModel } from './post.model';
-import {PostObject} from './post.alias';
-import { ApiTags, ApiBearerAuth, ApiOperation, ApiOkResponse, ApiResponse} from '@nestjs/swagger';
+import { PostObject } from './post.alias';
+import {
+    ApiTags,
+    ApiBearerAuth,
+    ApiOperation,
+    ApiOkResponse,
+    ApiResponse,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwtAuth.guard';
 import { PostLikeService } from './postLike/postLIke.service';
 import { PostLikeModel } from './postLike/postLike.model';
-import { PostLike, Comment } from '@prisma/client';
-import { PostLikeResponse, PostLikeStatus } from './postLike/types/postLike.types';
-import {AuthenticatedRequest} from 'src/interfaces/authenticatedRequest.interface'
+import { PostLike, Comment, PostTag } from '@prisma/client';
+import {
+    PostLikeResponse,
+    PostLikeStatus,
+} from './postLike/types/postLike.types';
+import { AuthenticatedRequest } from 'src/interfaces/authenticatedRequest.interface';
 import { isEmpty } from 'lodash';
 import { PostCommentService } from './postComment/postComment.service';
 import { PostCommentModel } from './postComment/postComment.model';
 import { AllowedActionGuard } from 'src/guards/allowedAction.guard';
+import { UserService } from '../user/user.service';
+import { UserTagService } from '../user/userTag/userTag.service';
+import { PostTagService } from '../post-tag/postTag.service';
 @Controller('post')
-@ApiTags("post")
+@ApiTags('post')
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
 export class PostController {
-  constructor(private readonly postService: PostService, private readonly PostLikeService: PostLikeService, private PostCommentService: PostCommentService) {}
+    constructor(
+        private readonly postService: PostService,
+        private readonly PostLikeService: PostLikeService,
+        private PostCommentService: PostCommentService,
+        private readonly UserService: UserService,
+        private UserTagService: UserTagService,
+        private PostTagService: PostTagService,
+    ) {}
 
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() post: PostModel, @Req() req: AuthenticatedRequest): Promise<PostObject> {
-    let response: PostObject;
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(
+        @Body() post: PostModel,
+        @Req() req: AuthenticatedRequest,
+    ): Promise<PostObject> {
+        let response: PostObject;
 
-    post.id_user = req.user.id_user || post.id_user;
-    
-    try {
-      response = await this.postService.create({
-      ...post
+        post.id_user = req.user.id_user || post.id_user;
+
+        try {
+            response = await this.postService.create({
+                ...post,
+            });
+        } catch (err: any) {
+            throw new NotFoundException(err);
+        }
+        return response;
+    }
+
+    /**
+     * @description get all posts on the website
+     * @returns {Promise<PostObject[]>}
+     */
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ description: 'Get all posts on the website' })
+    @ApiBearerAuth()
+    async getPosts(): Promise<PostObject[]> {
+        //should we filter posts by tags he likes?
+        return await this.postService.findAll();
+    }
+
+    @Get('/feed')
+    @ApiOperation({
+        description:
+            'Get all posts for  currenctly logged in user based on his liked tags',
     })
-  } catch(err: any) {
-      throw new NotFoundException(err); 
-  }
-  return response; 
-  }
+    @UseGuards(JwtAuthGuard)
+    @UseGuards(AllowedActionGuard) //NOTE: we need to feed only the user based on his preferences, noone else can see specific users feed
+    @ApiBearerAuth()
+    async getPostsForUser(
+        @Req() req: AuthenticatedRequest,
+    ): Promise<PostObject[]> {
+        //check if user has any liked tags
+        let user = await this.UserService.findOne({
+            id_user: req.user.id_user,
+        });
+        if (!user) throw new NotFoundException('User not found'); //don't really need to do this since we are using guards
+        //find all user tags
+        let userTags = await this.UserTagService.findAll(user.id_user);
+        console.log('User tags: ', userTags);
+        let posts: PostObject[] = await this.postService.findAll();
 
-  
-  /**
-   * 
-   * @returns {Promise<PostObject[]>} get all posts depending on the user tags if he has any  
-   */
-  @Get()
-  @UseGuards(JwtAuthGuard)
-  async getPosts(): Promise<PostObject[]> {
-    //should we filter posts by tags he likes? 
-      return await this.postService.findAll();
-    
-  
+        //if tags are empty, just return all posts since we don't filter by tags
+        if (!isEmpty(userTags)) {
+            //filter posts by tags
+            for (let post of posts) {
+                let postTags = await this.PostTagService.findAll(post.id_post);
 
+                let postTagsIds = postTags.map((tag) => tag.id_tag);
+                let userTagsIds = userTags.map((tag) => tag.id_tag);
+                let commonTags = postTagsIds.filter((tag) =>
+                    userTagsIds.includes(tag),
+                );
 
-  }
+                if (isEmpty(commonTags)) {
+                    posts = posts.filter((p) => p.id_post != post.id_post);
+                }
+            }
 
-  @Get(":id_post")
-  async getPost(@Param('id_post', ParseIntPipe) id_post: number): Promise<PostObject> {
-    return await this.postService.findOne({
-      id_post
+            //if(isEmpty(tags)) throw new NotFoundException("User has no liked tags");
+        }
+        return posts;
+    }
+
+    @Get(':id_post')
+    async getPost(
+        @Param('id_post', ParseIntPipe) id_post: number,
+    ): Promise<PostObject> {
+        return await this.postService.findOne({
+            id_post,
+        });
+    }
+
+    @Post('/like')
+    @UseGuards(AllowedActionGuard)
+    @UseGuards(JwtAuthGuard)
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOkResponse({
+        description:
+            'if post is already liked, the API will unlike the post and delete the record. Returns the status of the post like and the post like object. ',
+        type: PostLikeResponse,
     })
+    async likePost(
+        @Body() postLike: PostLikeModel,
+        @Req() req: AuthenticatedRequest,
+    ): Promise<PostLikeResponse> {
+        let response: PostLikeResponse = {
+            status: PostLikeStatus.LIKE,
+            postLike: null,
+        };
+        let postLikeCreated: PostLike;
 
-  }
+        //deduct id user from the token;
+        postLike.id_user = req.user.id_user || postLike.id_user;
+        console.log(postLike);
+        postLikeCreated = await this.PostLikeService.findOne({
+            id_post: postLike.id_post,
+            id_user: postLike.id_user,
+        });
+        if (!isEmpty(postLikeCreated)) {
+            await this.PostLikeService.delete({
+                id_post: postLike.id_post,
+                id_user: postLike.id_user,
+            });
+            response.status = PostLikeStatus.UNLIKE;
+            response.postLike = postLikeCreated;
+            return response;
+        }
 
-  @Post('/like')
-  @UseGuards(AllowedActionGuard)
-  @HttpCode(HttpStatus.CREATED)
-  @ApiOkResponse({description: "if post is already liked, the API will unlike the post and delete the record. Returns the status of the post like and the post like object. ", type: PostLikeResponse})
-  async likePost(@Body() postLike: PostLikeModel, @Req() req: AuthenticatedRequest): Promise<PostLikeResponse> {
-    
-    let response: PostLikeResponse = {
-      status: PostLikeStatus.LIKE,
-      postLike: null
-    };
-    let postLikeCreated: PostLike; 
-    
-    console.log("req.user.id_user", req.user); 
-    //deduct id user from the token; 
-    postLike.id_user = req.user.id_user || postLike.id_user;
-    console.log(postLike);
-      postLikeCreated = await this.PostLikeService.findOne({
-        id_post: postLike.id_post,
-        id_user: postLike.id_user
-      })
-      if(!isEmpty(postLikeCreated)) {
-        await this.PostLikeService.delete({
-          id_post: postLike.id_post,
-          id_user: postLike.id_user
-        })
-        response.status = PostLikeStatus.UNLIKE;
+        try {
+            postLikeCreated = await this.PostLikeService.create({
+                ...postLike,
+            });
+        } catch (err: any) {
+            throw new NotFoundException(err);
+        }
+        response.status = PostLikeStatus.LIKE;
+
+        //if user wants to like the same post, we want to remove like from the post based on the body - response would be the same
         response.postLike = postLikeCreated;
         return response;
-      }
-    
-
-    try {
-      postLikeCreated = await this.PostLikeService.create({
-        ...postLike
-        
-      });
-
-      
     }
-    catch(err: any) {
-      throw new NotFoundException(err);
-    }
-    response.status = PostLikeStatus.LIKE; 
-
-    //if user wants to like the same post, we want to remove like from the post based on the body - response would be the same
-      response.postLike = postLikeCreated;
-      return response;
-    
-    
-
-  }
-
- 
-
-  
 }
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,111 +1,215 @@
 import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { ServiceInterface } from 'src/interfaces/service.interface';
-import {Post, Prisma} from '@prisma/client';
+import { Post, Prisma } from '@prisma/client';
 import { PostModel } from './post.model';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from '../user/user.service';
 import { PostTagService } from 'src/modules/post-tag/postTag.service';
 @Injectable()
-export class PostService implements ServiceInterface<Post>{
-    constructor(private PrismaService: PrismaService,
-       @Inject(forwardRef(() => UserService)) private UserService: UserService,
-       private PostTagService: PostTagService) {}
+export class PostService implements ServiceInterface<Post> {
+    constructor(
+        private PrismaService: PrismaService,
+        @Inject(forwardRef(() => UserService)) private UserService: UserService,
+        private PostTagService: PostTagService,
+    ) {}
 
     async findAll(): Promise<Post[]> {
-        throw new Error('Method not implemented.');
-    }
-
-
-    async findAllByIdUser(id_user: number): Promise<Post[]> {
         return await this.PrismaService.post.findMany({
-                where: {
-                    id_user
-                }
-                
-            
-        })
-    }
-
-
-    async findOne(where: Prisma.PostWhereInput): Promise<Post | null> {
-        return await this.PrismaService.post.findFirst({
-            where: {
-                id_post: where.id_post
-
-            },
             include: {
+                user: {
+                    select: {
+                        username: true,
+                        id_user: true,
+                        details: {
+                            select: {
+                                image: true,
+                            },
+                        },
+                    },
+                },
                 comments: {
                     select: {
                         id_comment: true,
                         content: true,
                         createdAt: true,
-                        likes: true,
                         user: {
                             select: {
                                 id_user: true,
                                 username: true,
                                 details: {
                                     select: {
-                                        image: true
-                                    }
-                                }
-
-                            }
-                        }
-                    }
+                                        image: true,
+                                    },
+                                },
+                            },
+                        },
+                        likes: {
+                            select: {
+                                user: {
+                                    select: {
+                                        id_user: true,
+                                        username: true,
+                                        details: {
+                                            select: {
+                                                image: true,
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
                 },
-                likes: true,
+                likes: {
+                    select: {
+                        user: {
+                            select: {
+                                id_user: true,
+                                username: true,
+                                details: {
+                                    select: {
+                                        image: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+
                 PostTag: {
-                    select:{
-                        
+                    select: {
+                        tag: {
+                            select: {
+                                name: true,
+                                id_tag: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+    }
+
+    async findAllByIdUser(id_user: number): Promise<Post[]> {
+        return await this.PrismaService.post.findMany({
+            where: {
+                id_user,
+            },
+        });
+    }
+
+    async findOne(where: Prisma.PostWhereInput): Promise<Post | null> {
+        return await this.PrismaService.post.findFirst({
+            where: {
+                id_post: where.id_post,
+            },
+
+            include: {
+                user: {
+                    select: {
+                        username: true,
+                        id_user: true,
+                        details: {
+                            select: {
+                                image: true,
+                            },
+                        },
+                    },
+                },
+                comments: {
+                    select: {
+                        id_comment: true,
+                        content: true,
+                        createdAt: true,
+                        likes: {
+                            select: {
+                                user: {
+                                    select: {
+                                        username: true,
+                                        id_user: true,
+                                    },
+                                },
+                                id_comment_like: true,
+                            },
+                        },
+                        user: {
+                            select: {
+                                id_user: true,
+                                username: true,
+                                details: {
+                                    select: {
+                                        image: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                likes: {
+                    select: {
+                        user: {
+                            select: {
+                                username: true,
+                                id_user: true,
+                                details: {
+                                    select: {
+                                        image: true,
+                                    },
+                                },
+                            },
+                        },
+                        createdAt: true,
+                        id_post_like: true,
+                    },
+                },
+                PostTag: {
+                    select: {
                         id_tag: false,
                         id_post: false,
-                        tag: true,
+                        tag: {
+                            select: {
+                                id_tag: true,
+                                name: true,
+                            },
+                        },
                     },
-                    
                 },
                 image: true,
-            }
-        }); 
+            },
+        });
     }
 
     async create(data: PostModel): Promise<Post> {
-
-        //todo: upload image to post 
-        const {content, id_user, tags} = data;
+        //todo: upload image to post
+        const { content, id_user, tags } = data;
 
         const userExists = await this.UserService.findOne({
-            id_user
+            id_user,
         });
-        if(!userExists) throw new Error('User not found!'); 
+        if (!userExists) throw new Error('User not found!');
 
-        let postCreated: Post = null; 
-
+        let postCreated: Post = null;
 
         postCreated = await this.PrismaService.post.create({
             data: {
                 content: content,
                 user: {
                     connect: {
-                        id_user: id_user
-                    }
+                        id_user: id_user,
+                    },
                 },
+            },
+        });
 
-            }, 
-            
-        })
+        if (!postCreated) throw new Error('Error on create post');
 
-        if(!postCreated) throw new Error('Error on create post');
+        //connect post tags
 
-        //connect post tags 
-        
-        await this.PostTagService.create(tags, postCreated); 
+        await this.PostTagService.create(tags, postCreated);
 
         return this.findOne({
-            id_post: postCreated.id_post
-        }); 
-
-
+            id_post: postCreated.id_post,
+        });
     }
     async update(params: {
         where: Prisma.PostWhereUniqueInput;
