14895985a66cda295be36bbc628bbd12463b0076
Marek <sarkowag@gmail.com>
2023-05-14 20:27:53 +0200

    refactor

--- apps/notifications-service/test/app.e2e-spec.ts
+++ apps/notifications-service/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { NotificationsServiceModule } from './../src/notifications-service.module';
 
-describe('AppController (e2e)', () => {
+describe('NotificationsServiceController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [NotificationsServiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
