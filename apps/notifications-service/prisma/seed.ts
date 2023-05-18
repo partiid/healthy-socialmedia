40ab629c33f83f8f47c552142db405c4ce5264fb
Marek <sarkowag@gmail.com>
2023-05-18 21:51:58 +0200

    prisma seed & docker-compose mongo replSet

--- apps/notifications-service/prisma/seed.ts
+++ apps/notifications-service/prisma/seed.ts
@@ -1,26 +1,44 @@
 import actions from "../data/actions";
+import resources from "../data/resources";
 import { PrismaClient } from "@prisma/notifications-client";
 const Prisma = new PrismaClient(); 
 
 
 const seedResources = async () => {
-    for(let action of actions) {
+    for(let resource of resources) {
         await Prisma.resource.create({
             data: {
-                name: action.name
+                name: resource.name
             }
         })
     }
 }
 
+const getIdResource = async (name: string) => {
+    let resource = await Prisma.resource.findFirst({
+        where: {
+            name
+        }
+    });
+    return resource.id_resource;
+}
+
 const seedActions = async () => {
+
     for(let action of actions) {
+
+        //splitting the name by . and getting the resource name
+        //then get the id_resource from the resource table
+        console.log(action.name.split('.')[0]); 
+        let id_resource = await getIdResource(action.name.split('.')[0]);
+        
+
         await Prisma.action.create({
             data: {
                 name: action.name,
                 resources: {
                     connect: {
-                        id_resource: action.id_resource
+                        id_resource: id_resource.toString()
                     }
                 }
             }
@@ -29,8 +47,10 @@ const seedActions = async () => {
 }
 
 const seed = async () => {
-    await seedResources();
-    await seedActions();
+    await seedResources().then(async () => {
+        await seedActions();
+
+    });
 
 }
 
